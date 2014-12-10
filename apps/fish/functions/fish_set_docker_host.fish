c91804a0954fa1e15fa582f72f1ac1376e54cc1b
Walker Lee <walkerlee.tw@gmail.com>
2014-12-10 15:33:19 +0800

    Improve boot2docker env

--- apps/fish/functions/fish_set_docker_host.fish
+++ apps/fish/functions/fish_set_docker_host.fish
@@ -1,7 +1,5 @@
 function fish_set_docker_host --description "Set DOCKER_HOST variable"
-	if test -f (which boot2docker)
-		set -gx DOCKER_HOST (boot2docker socket 2>/dev/null)
-		set -gx DOCKER_CERT_PATH $HOME/.boot2docker/certs/boot2docker-vm
-		set -gx DOCKER_TLS_VERIFY 1
+	for cmd in (boot2docker shellinit 2>/dev/null)
+		eval $cmd
 	end
 end
