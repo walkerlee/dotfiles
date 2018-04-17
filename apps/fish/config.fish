93ab56ca02bb4e2f895f3c048d6c6562308c3f81
Walker Lee <walkerlee.tw@gmail.com>
2018-04-17 21:39:19 +0800

    update fish config

--- apps/fish/config.fish
+++ apps/fish/config.fish
@@ -1,14 +1,3 @@
-# Path to Oh My Fish install.
-set -q XDG_DATA_HOME
-  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
-  or set -gx OMF_PATH "$HOME/.local/share/omf"
-
-# Customize Oh My Fish configuration path.
-#set -gx OMF_CONFIG "/Users/walkerlee/.config/omf"
-
-# Load oh-my-fish configuration.
-source $OMF_PATH/init.fish
-
 # Environment variable
 
 # Make vim the default editor
--- bin/install.sh
+++ bin/install.sh
@@ -5,10 +5,9 @@ APPS_PATH=$BASE_PATH/apps
 
 # remove dotfiles
 rm -f  $HOME/{.bash_profile,.bashrc}                # bash
-rm -r  $HOME/.config/fish/config.fish               # fish
+rm -r  $HOME/.config/fish/conf.d/config.fish        # fish
 rm -f  $HOME/{.gitconfig,.gitignore_global}         # git
 rm -f  $HOME/.hgignore_global                       # mercurial
-rm -rf $HOME/.local/share/omf                       # oh-my-fish
 rm -rf $HOME/{.vim,.vimrc}                          # vim
 
 # create essential folder(s)
@@ -17,11 +16,10 @@ mkdir -p $HOME/{.config/fish,.local/share}
 # link dotfiles
 ln -s $APPS_PATH/bash/bash_profile         $HOME/.bash_profile
 ln -s $APPS_PATH/bash/bashrc               $HOME/.bashrc
-ln -s $APPS_PATH/fish/config.fish          $HOME/.config/fish/config.fish
+ln -s $APPS_PATH/fish/config.fish          $HOME/.config/fish/conf.d/config.fish
 ln -s $APPS_PATH/git/gitconfig             $HOME/.gitconfig
 ln -s $APPS_PATH/git/gitignore_global      $HOME/.gitignore_global
 ln -s $APPS_PATH/mercurial/hgignore_global $HOME/.hgignore_global
-ln -s $APPS_PATH/oh-my-fish                $HOME/.local/share/omf
 ln -s $APPS_PATH/vim                       $HOME/.vim
 ln -s $APPS_PATH/vim/vimrc                 $HOME/.vimrc
 
